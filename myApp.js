acd580c837713398d8a0b959e1dc79c9c7a191dd
Alvin <alvin.tol@hotmail.com>
2022-08-04 18:52:54 -0600

    adds post route

--- myApp.js
+++ myApp.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   path = __dirname + '/views/index.html';
 
   res.sendFile(path)
 })
-app.use('/public', express.static(__dirname + '/public'))
 
 app.use('*', (req, res, next) => {
   express.static('root',
@@ -39,8 +41,10 @@ app.get('/name', (req, res) => {
   res.send({ name: `${first} ${last}` })
 });
 
-
-
+app.post('/name', (req, res) => {
+  const { first, last } = req.body;
+  res.send({ name: `${first} ${last}` })
+})
 
 
 
